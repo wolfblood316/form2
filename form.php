9db97bb931dd1a51b4228683cb3b8dcbd181ae29
wolfblood316 <adrianlandin@yahoo.com>
2014-12-01 22:16:53 -0500

    added required tag

--- form.php
+++ form.php
@@ -45,11 +45,11 @@
 			<fieldset>
 				<p>
 					<label for="name">Name:</label><?php echo @$name_error; ?>
-					<input type="text" id="name" name="name" value="<?php echo @$name ?>" class="required" />
+					<input type="text" id="name" name="name" value="<?php echo @$name ?>" class="required" / required>
 				</p>
 				<p>
 					<label for="email">Email:</label><?php echo @$email_error; ?>
-					<input type="email" id="email" name="email" value="<?php echo @$email ?>" class="email required" />
+					<input type="email" id="email" name="email" value="<?php echo @$email ?>" class="email required" / required>
 				</p>
 				<p>
 					<label for="message">Message:</label><?php echo @$message_error; ?>
